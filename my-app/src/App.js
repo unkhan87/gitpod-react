ccea0dfc36078344f64a715502c3cacedef0885e
unkhan87 <ukhan87@icloud.com>
2022-12-18 23:43:24 +0000

    Stateless functions and props completed

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = (props) => {
+console.log(props);
+return <h1>Hello, {props.name}! I see you're {props.age} years old; {props.greeting}</h1>;
+}
+export default FunctionalGreetingWithProps;
